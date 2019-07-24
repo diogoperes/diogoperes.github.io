d1e2c8f94db351ae1dd836b4137bada36c5e919f
diogoperes <diogo.peres@hotmail.com>
2019-07-24 21:41:24 +0100

    remove animation on skill bars for touch devices and change animation on portfolio for smaller width

--- src/js/animateOnLooking.js
+++ src/js/animateOnLooking.js
@@ -1,5 +1,17 @@
 jQuery(function($) {
 
+  var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+
+  if(!!supportsTouch) {
+    var $removeAnimations = $('.dontAnimateOnTouch');
+    $removeAnimations.css("animation", "none");
+    $removeAnimations.removeClass('animatable').addClass('animated');
+    console.log('oiadosaid')
+
+    $('body').addClass('touchDevice');
+
+  }
+
   // Function which adds the 'animated' class to any '.animatable' in view
   var doAnimations = function() {
 
@@ -32,6 +44,32 @@ jQuery(function($) {
 
   // Hook doAnimations on scroll, and trigger a scroll
 	$(window).on('scroll', doAnimations);
+
+  //optimize scroll
+  // var waiting = false, endScrollHandle;
+  // $(window).scroll(function () {
+  //   if (waiting) {
+  //     return;
+  //   }
+  //   waiting = true;
+  //
+  //   // clear previous scheduled endScrollHandle
+  //   clearTimeout(endScrollHandle);
+  //
+  //   doAnimations();
+  //
+  //   setTimeout(function () {
+  //     waiting = false;
+  //   }, 100);
+  //
+  //   // schedule an extra execution of scroll() after 200ms
+  //   // in case the scrolling stops in next 100ms
+  //   endScrollHandle = setTimeout(function () {
+  //     doAnimations();
+  //   }, 200);
+  // });
+
+
   $(window).trigger('scroll');
 
 });
